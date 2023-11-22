7d0a0a27d9901499b026b8ebe8b8cfe3efa48596
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-22 18:58:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
 
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.container');
 
+// Open rotating menu
+open.addEventListener('click',() => {
+    container.classList.add('show-nav');
+})
+
+// Close rotating menu
+close.addEventListener('click',() => {
+    container.classList.remove('show-nav');
+})
